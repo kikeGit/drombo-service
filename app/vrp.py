391499a6178a85fea1404289550198694f303d1f
kikeGit <67452251+kikeGit@users.noreply.github.com>
2025-06-14 20:13:02 -0300

    version final vrp

--- app/vrp.py
+++ app/vrp.py
@@ -125,34 +125,35 @@ class VRPTW_MultipleDeliveries:
         self.vehicle_capacity: int = vehicle_capacity
         self.max_vehicles: int = max_vehicles
 
-    def solve(self, unrouted_deliveries: List[Node], predefined_routes: List[List[Node]]):
+    def solve(self, unrouted_deliveries: List[Node], predefined_route: List[Node]):        
+        depot_copy = Node(0, 0, (0, 20), 0, 'Depot', None, None)
+        route = []
+        start_service = []
 
         if not unrouted_deliveries:
-            print("return predefined")
-            return predefined_routes, [], None
+            if len(predefined_route) > 0:
+                start_service = self.set_start_service_times(predefined_route) # pls
+                start_service.pop()
+            
+            return predefined_route, [], None, start_service
         
-        routes = []
+        #routes = []
         unrouted_deliveries_copy = unrouted_deliveries.copy()
         impossible_node = None
 
-        depot_copy = Node(0, 0, (0, 20), 0, 'Depot', None, None)
-        processed_routes = []
 
         while unrouted_deliveries_copy:
 
-            if predefined_routes:
+            if predefined_route:
 
-                predefined_r = predefined_routes[0].copy()
-                predefined_r.pop() #saca el depot del final
+                predefined_r = predefined_route.copy()
+                if len(predefined_r) > 1:
+                    predefined_r.pop() #saca el depot del final
                 route: List[Node] = predefined_r.copy()
-
-                #for node in route:
-                #    print('PREDEFINED ROUT:' + node.delivery_name)
 
                 loads_by_node = self.get_route_load(predefined_r)
                 boxes_by_node = self.get_route_boxes(predefined_r)
 
-                predefined_routes.pop(0)
             
             else:
                 boxes_info = {
@@ -169,16 +170,11 @@ class VRPTW_MultipleDeliveries:
                 best_delivery = None
                 best_cost = infinity
                 best_position = infinity
-                start_service = self.set_start_service_times(route, depot_copy) # pls
+                start_service = self.set_start_service_times(route) # pls
+                impossible_nodes: List[Node] = []
 
-                #print("start_service: ")
-                #print(start_service)
-                #print("----------------------------------------------------------------")
-                
                 for node in unrouted_deliveries_copy:
                     min_cost, local_position, impossible_node = self.min_insertion_cost(route, node, start_service, loads_by_node, boxes_by_node)
-                    #print("min_cost:" + str(min_cost))
-                    #print("min_cost:" + str(local_position))
                                             
                     if min_cost < best_cost:
                         
@@ -186,54 +182,50 @@ class VRPTW_MultipleDeliveries:
                         best_cost = min_cost
                         best_position = local_position
 
+                    if impossible_node:
+                        impossible_nodes.append(impossible_node)
+
                 if best_delivery is None:
                     break # termina la ruta
                 
                 self.update_loads_by_node(best_delivery, best_position, loads_by_node, route)
-                print("Agrego una nueva box a la ruta " + best_delivery.delivery_name + " " + best_delivery.box_type)
-                self.print_route(route)
-
                 self.update_boxes_by_node(best_delivery, best_position, boxes_by_node, route)
-                print("Nuevo boxes by node")
-                print(boxes_by_node)
-
-                route.insert(best_position, best_delivery)
                 
+                route.insert(best_position, best_delivery)
                 unrouted_deliveries_copy.remove(best_delivery)
             
             if len(route) == 1:
                 
-                if impossible_node:
-                    print("Quitar pedido imposible de los pedidos para rutear.")
-                    unrouted_deliveries_copy.remove(impossible_node)
+                for imp_node in impossible_nodes:
+                    print(imp_node)
+                    print("Quitar pedido imposible de los pedidos para rutear. " + imp_node.delivery_name)
+                    unrouted_deliveries_copy.remove(imp_node)
 
                 # Ruta de largo uno significa q solo pude agregar el deposito en una nueva ruta, ya no tengo tiempo para seguir creando rutas este dia
-                print("Aca no es posible seguir ruteando pedidos de esta prioridad.")
+                #print("Aca no es posible seguir ruteando pedidos de esta prioridad.")
                 break
 
             else:
                 # terminamos, intentamos empezar otra, actualizamos el depot
-                # (U, L, L, L, L - D - U)
-
-                route.append(self.depot) # metemos el ultimo depot.
-                #self.print_route(route)
-                routes.append(route) 
-
-                last_node_index_before_depot = route[len(route) - 2]
-                second_depot_index = len(route) - 1
                 
-                last_start_service = self.get_start_service_time_for_node(last_node_index_before_depot, self.depot, start_service, second_depot_index)
-                depot_copy.time_window = (last_start_service, depot_copy.time_window[1])
+                final_depot = Node(0, 0, (0, 20), 0, 'Depot', None, None)
+                last_node_index_before_depot = route[len(route) - 1]
+                ultimo = len(route)
+                
+                last_start_service = self.get_start_service_time_for_node(last_node_index_before_depot, self.depot, start_service, ultimo)
+                final_depot.time_window = (last_start_service, depot_copy.time_window[1])
+                
+                route.append(final_depot) # metemos el ultimo depot.
+                
+                break
         
-        #print("---------------------------------------------------------------------------------")
-        return routes, unrouted_deliveries_copy, impossible_node
+        return route, unrouted_deliveries_copy, impossible_node, start_service
 
     def print_route(route: List[Node]):
         str_to_print = ''
         for j in range(len(route)):
             node = route[j]
             str_to_print += node.delivery_name + '->'
-        #print(str_to_print)
         return str_to_print 
     
     def update_boxes_by_node(self, best_delivery: Node, best_position, boxes_by_node, route):
@@ -283,7 +275,6 @@ class VRPTW_MultipleDeliveries:
 
     def update_box_object(self, box_type, box_object: dict, sum: bool):
         # si sum es true, sumo 1. si false resto 1
-        #print(box_object)
         new_box_object = {
             "SMALL": box_object["SMALL"],
             "MEDIUM": box_object["MEDIUM"],
@@ -311,6 +302,7 @@ class VRPTW_MultipleDeliveries:
         return new_box_object
 
     def update_loads_by_node(self, best_delivery: Node, best_position: int, loads_by_node, route: List[Node]):
+        
         if best_delivery.delivery_type == 'pedido':
             for i in range(0, best_position):
                 loads_by_node[i] = loads_by_node[i] + best_delivery.demand
@@ -319,7 +311,7 @@ class VRPTW_MultipleDeliveries:
             loads_by_node.insert(best_position, new_load)
 
         if best_delivery.delivery_type == 'envio':
-            for i in range(best_position, len(route) - 1): # sacar el menos 1
+            for i in range(best_position, len(route)): # sacar el menos 1
                 loads_by_node[i] = loads_by_node[i] + best_delivery.demand
 
             new_load = loads_by_node[best_position - 1] + best_delivery.demand # loads by node (2)
@@ -399,7 +391,7 @@ class VRPTW_MultipleDeliveries:
         min_cost = infinity
         best_position = infinity 
 
-        for i in range(len(route)):
+        for i in range(len(route)): # revisar posiciones 0 y 1 ? 
             previous_node = route[i]
             next_node = self.depot if (i == len(route) - 1) else route[i+1]
 
@@ -409,8 +401,6 @@ class VRPTW_MultipleDeliveries:
                 (alfa_3) * self.c13(previous_node, unrouted_node, None, start_service, i+1) / time_window_normalizer
             )
            
-            #print("local" + str(local_solution))
-
             if local_solution < min_cost:
 
                 is_feasible_push_forward, impossible_node = self.check_push_forward(route, unrouted_node, i+1, start_service)
@@ -428,8 +418,8 @@ class VRPTW_MultipleDeliveries:
                     is_feasible_box = self.check_pedido_box(route, unrouted_node, i+1, boxes_by_node)
 
                 if unrouted_node.delivery_type == 'envio':
-                    is_feasible_load = self.check_envio_load(route, unrouted_node, i+1, loads_by_node)
-                    is_feasible_box = self.check_envio_box(route, unrouted_node, i+1, boxes_by_node)
+                    is_feasible_load = self.check_envio_load(route, unrouted_node, i, loads_by_node)
+                    is_feasible_box = self.check_envio_box(route, unrouted_node, i, boxes_by_node)
 
                 
                 if is_feasible_box and is_feasible_load and is_feasible_push_forward:
@@ -453,7 +443,7 @@ class VRPTW_MultipleDeliveries:
         return True
 
     def check_envio_load(self, route, unrouted_node, position, loads_by_node):
-        for i in range(position, len(route)):
+        for i in range(position, len(route)): # checkear las posciones tenemos problemas con
             if loads_by_node[i] + unrouted_node.demand > self.vehicle_capacity:
                 return False
         
@@ -480,14 +470,7 @@ class VRPTW_MultipleDeliveries:
                 return False
         
         return True
-    
-    def check_envio_load(self, route, unrouted_node, position, loads_by_node):
-        for i in range(position, len(route)):
-            if loads_by_node[i] + unrouted_node.demand > self.vehicle_capacity:
-                return False
-        
-        return True
-    
+
     def check_push_forward(self, route: List[Node], node: Node, position, start_service):
 
         distance_to_node = self.distance_between_nodes(route[position-1], node)
@@ -513,11 +496,12 @@ class VRPTW_MultipleDeliveries:
             feasible_insertion = (bj + PF) <= next_policlinic.get_end_time_window()
             if not feasible_insertion:
                 # caso borde pedido imposible
-                if len(route) == 1 and route[0].get_start_time_window() > self.depot.get_start_time_window(): #tremendo bugfix
+                if len(route) == 1 and route[0].get_start_time_window() == self.depot.get_start_time_window():
+                    return False, node
+                if len(route) == 1 and (bj + PF) > self.depot.get_end_time_window(): #probablemente no es necesario y nunca entre
                     return False, node
 
                 return False, None
-
         for j in range(position + 1, len(route) + 1): 
             next_policlinic = route[j] if j < len(route) else self.depot
             previous_node = route[j-1]
@@ -536,8 +520,8 @@ class VRPTW_MultipleDeliveries:
 
         return True, None
 
-    def set_start_service_times(self, route: List[Node], current_depot: Node):
-        start_service = [current_depot.get_start_time_window()]
+    def set_start_service_times(self, route: List[Node]):
+        start_service = [route[0].get_start_time_window()]
 
         for j in range(1, len(route)): # [depot]
             previous_node = route[j-1]
@@ -555,93 +539,3 @@ class VRPTW_MultipleDeliveries:
         
         start_service.append(return_to_depot)
         return start_service
-
-def __main__():
-    
-    
-    depot = Node(0, 0, (0, 20), 0, 'Depot', None)
-
-    urgent_deliveries = [
-        Node(4, 400, (15, 20), 0.7, 'J', 'envio', "SMALL"),
-        Node(1, 1500, (10, 13), 0.5, 'B', 'pedido', "BIG"),
-        Node(6, 2800, (12, 19), 0.7, 'P', 'pedido', "SMALL"),
-        Node(9, 1000, (9, 18), 0.7, 'M', 'envio', "SMALL"),
-        Node(4, 1200, (12, 15), 0.7, 'N', 'pedido', "MEDIUM"),
-    ] 
-
-    normal_deliveries = [
-        Node(2, 1500, (10, 14), 0.6, 'D', 'pedido', "MEDIUM"),
-        Node(3, 1900, (9, 15), 0.8, 'E', 'pedido', "MEDIUM"),
-        Node(4, 900, (11, 17), 0.5, 'F', 'pedido', "BIG"),
-        Node(5, 2500, (7, 10), 0.7, 'G', 'pedido', "SMALL"),
-    ] 
-
-    low_deliveries = [
-        Node(1, 200, (9, 12), 0.5, 'A', 'pedido', "MEDIUM"),
-        Node(2, 400, (8, 11), 0.7, 'C', 'pedido', "BIG"),
-        Node(5, 1500, (9, 12), 0.7, 'H', 'pedido', "MEDIUM"),
-        Node(6, 400, (7, 10), 0.7, 'I', 'pedido', "BIG"),
-        Node(8, 400, (13, 16), 0.7, 'K', 'pedido, "MEDIUM"'),
-        Node(8, 400, (13, 20), 0.7, 'L', 'envio', "SMALL"),
-        Node(5, 400, (8, 19), 0.7, 'O', 'pedido', "SMALL"),
-        Node(1, 400, (9, 10), 0.7, 'Q', 'pedido', "SMALL"),
-        Node(9, 100, (18, 21), 0.7, 'Mariana', 'pedido', "SMALL"),
-    ]
-            
-    deliveries: List[Node] = [
-        #Node(1, 200, (10, 13), 0.5, 'B'),
-        #Node(1, 300, (9, 12), 0.5, 'A'),
-        #Node(2, 400, (8, 11), 0.7, 'C'),
-        #Node(2, 200, (10, 14), 0.6, 'D'),
-        #Node(3, 500, (9, 15), 0.8, 'E'),
-        #Node(4, 300, (11, 17), 0.5, 'F'),
-        #Node(5, 400, (7, 10), 0.7, 'G'),
-        #Node(5, 1500, (9, 12), 0.7, 'H'),
-        #Node(6, 400, (7, 10), 0.7, 'I'),
-        #Node(7, 400, (15, 20), 0.7, 'J'),
-        #Node(8, 400, (13, 16), 0.7, 'K'),
-        #Node(8, 400, (13, 20), 0.7, 'L'),
-        #Node(9, 400, (9, 18), 0.7, 'M'),
-        #Node(4, 400, (12, 15), 0.7, 'N'),
-        #Node(5, 400, (8, 19), 0.7, 'O'),
-        #Node(6, 400, (12, 19), 0.7, 'P'),
-        #Node(1, 400, (9, 10), 0.7, 'Q'),
-    ]
-
-    varp = VRPTW_MultipleDeliveries(deliveries, depot, distances, vehicle_capacity, max_vehicles)
-    routes: List[List[Node]] = []
-    routes_by_day = {}
-    i = 1
-    while len(urgent_deliveries) != 0 or len(normal_deliveries) != 0 or len(low_deliveries) != 0:
-        
-        routes, urgent_deliveries = varp.solve(urgent_deliveries, routes)
-        print("Finaliza ruteo Urgente" )
-        routes, normal_deliveries = varp.solve(normal_deliveries, routes)
-        print("Finaliza ruteo Normal" )
-        routes, low_deliveries = varp.solve(low_deliveries, routes)
-        print("Finaliza ruteo Low" )
-
-        if len(routes) != 0:
-            routes_copy = []
-            for route in routes:
-                route_copy = route.copy()
-                routes_copy.append(route_copy)
-
-
-            routes_by_day[i] = routes_copy.copy()
-            print("Aca termina el día " + str(i))
-            i += 1
-            routes = []
-
-
-    #routes_by_day = defaultdict(list)
-    for key in routes_by_day.keys():
-        routes = routes_by_day.get(key)
-        print('Routes for day ' + str(key) + ':')
-        #print(routes)
-        for route in routes:
-           varp.print_route(route)
-        print('-----------------------------------------------------------------------------')
-
-
-#__main__()
