de8d2e23665bfb22f346a0fd8f0722907ea3d4b0
Mariana Bartesaghi <maribartesa@gmail.com>
2025-07-03 20:14:12 -0300

    Add estimated date to transfer

--- app/models.py
+++ app/models.py
@@ -116,6 +116,8 @@ class Transfer(db.Model):
             "operation_id": self.operation_id,
             "weight": self.weight,
             "supplies": [s.to_dict() for s in self.supplies],
+            "estimated_arrival_date": self.estimated_arrival_date.isoformat(),
+            "estimated_arrival_time": self.estimated_arrival_time.isoformat(),
         }
 
 
